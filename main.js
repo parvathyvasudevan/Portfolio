3ab416f3982d9430d59adff0a0a8f6b51539eeb1
PARVATHY VASUDEVAN <118750444+parvathyvasudevan@users.noreply.github.com>
2025-06-14 22:17:43 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text", {
-    strings: ["Frontend Developer", "DevOps Engineer", "Web Developer"],
+    strings: ["Frontend Developer","Web Developer"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
